cfb737aada8a97d2732384952231420cef3754f2
vishalpwaman <vishalpwaman1997@gmail.com>
2020-04-20 15:59:00 -0400

    Glamber Usecase 5

--- glamber.sh
+++ glamber.sh
@@ -6,28 +6,41 @@ MaxMoney=150
 MinMoney=50
 Win=1
 i=0
-for (( C=0 ; C<20 ; C++ ))
-do	
-	Money=$Stack
-	WinPerDay=0
-	LossPerDay=0
-	while (( $MaxMoney > $Money && $MinMoney < $Money ))
-	do 
-		check=$(($RANDOM%2))
-		if (( $check == $Win ))
-		then
-			WinPerDay=$(($WinPerDay + 1))
-			Money=$(($Money+1))
-		else
-			LossPerDay=$(($LossPerDay + 1))
-			Money=$(($Money-1))
-		fi
+for (( M=0 ; M<12 ; M++ ))
+do
+	for (( C=0 ; C<30 ; C++ ))
+	do	
+		Money=$Stack
+		WinPerDay=0
+		LossPerDay=0
+		while (( $MaxMoney > $Money && $MinMoney < $Money ))
+		do 
+			check=$(($RANDOM%2))
+			if (( $check == $Win ))
+			then
+				WinPerDay=$(($WinPerDay + 1))
+				Money=$(($Money+1))
+			else
+				LossPerDay=$(($LossPerDay + 1))
+				Money=$(($Money-1))
+			fi
+		done
+        	echo "Win : $WinPerDay  Loss : $LossPerDay"
+		WinArray[$M,$C]=$WinPerDay
+		LossArray[$M,$C]=$LossPerDay 
 	done
-        echo "Win : $WinPerDay  Loss : $LossPerDay"
-	WinArray[$C]=$WinPerDay
-	LossArray[$C]=$LossPerDay 
-
 done
-
-echo "Win Per Day  : ${WinArray[@]}"
-echo "Loss Per DAy : ${LossArray[@]}"
+for (( p=0 ;p<12 ;p++ ))
+do
+	echo "Win in $(($p+1)) Month"
+	for(( q=0 ; q<30 ; q++ ))
+	do
+		echo "DAY $q :${WinArray[$p,$q]}"
+	done
+	
+	echo "Loss in $(($p+1)) Month"
+	for(( q=0 ; q<30 ; q++ ))
+	do
+		echo "DAY $q :${LossArray[$p,$q]}"
+	done
+done
